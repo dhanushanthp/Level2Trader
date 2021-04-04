9197363a01365bdd357bdb13b20206da93791089
Dhanushanth <dhanushanthp@gmail.com>
2021-04-04 17:00:20 +0800

    Removed old files

--- bidask.sh
+++
@@ -1,6 +0,0 @@
-max=100
-number_of_records=6
-for l in $(seq 1 $max); do
-  echo "$l"
-  python monitoring/bid_and_ask/bidask_api_call.py "$1" "$number_of_records"
-done
--- time_and_sales.sh
+++
@@ -1,6 +0,0 @@
-max=100
-number_of_records=10
-for l in $(seq 1 $max); do
-  echo "$l"
-  python monitoring/time_and_sales/time_and_sales_api_call.py "$1" "$number_of_records"
-done
