bc9fc02e5b802eea467335aae00787bb055b20aa
Dhanushanth <dhanushanthp@gmail.com>
2021-04-09 13:22:26 +0800

    Added Chart to track overall sales and high values
    Signed-off-by: Dhanushanth <dhanushanthp@gmail.com>

--- src/visualizer/all_time_sales.py
+++ src/visualizer/all_time_sales.py
@@ -5,7 +5,7 @@ from matplotlib.animation import FuncAnimation
 from numerize.numerize import numerize
 
 plt.style.use('ggplot')
-chart_width = 15
+chart_width = 10
 chart_height = 5
 plt.figure(figsize=(chart_width, chart_height))
 plt.rcParams['toolbar'] = 'None'
@@ -18,24 +18,26 @@ def animate(i):
     # Ratio, If we use 0.5 then it will be bar without gaps since we have 2 bar side by size. The max value is 1 same as 100% space for bar
     bar_width = 0.45
 
-    x = data['price'].astype(str)
-    y1 = data['bid']
-    y2 = data['ask']
+    y = data['price'].astype(str)
+    x1 = data['bid']
+    x2 = data['ask']
     plt.cla()
     # x coordinated of the bar
-    bar_bids = plt.bar(x=records_idx + bar_width, height=y1, width=bar_width, label='On BID', color='tab:red')
-    bar_ask = plt.bar(x=records_idx + bar_width + bar_width, height=y2, width=bar_width, label='On ASK', color='darkgreen')
+    bar_bids = plt.barh(y=records_idx + bar_width, width=x1, height=bar_width, label='On BID', color='tab:red')
+    bar_ask = plt.barh(y=records_idx + bar_width + bar_width, width=x2, height=bar_width, label='On ASK', color='darkgreen')
     # ticks: coordinates of the bar
-    plt.xticks(ticks=records_idx + bar_width + bar_width / 2, labels=x, fontsize=14)
+    plt.yticks(ticks=records_idx + bar_width + bar_width / 2, labels=y, fontsize=14)
+    plt.tick_params(axis='y', which='both', labelleft='off', labelright='on')
 
     # Annotate the bar with values
-    plt.bar_label(container=bar_bids, labels=[numerize(i) for i in y1], padding=3)
-    plt.bar_label(container=bar_ask, labels=[numerize(i) for i in y2], padding=3)
+    plt.bar_label(container=bar_bids, labels=[numerize(i) for i in x1], padding=-10)
+    plt.bar_label(container=bar_ask, labels=[numerize(i) for i in x2], padding=-10)
 
-    plt.legend(loc='upper right')
-    plt.xlabel('Price')
-    plt.ylabel('Number of shares')
-    plt.title('Total On Sales')
+    # plt.legend(loc='upper right')
+    plt.ylabel('Price')
+    plt.xlabel('Number of shares')
+    plt.title('Total Sales')
+    plt.gca().invert_xaxis()
     plt.tight_layout()
 
 
